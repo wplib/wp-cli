aedf48eb8b93b37a2d6c3bd83c4ed2b0e1a12e7d
Daryl Lozupone <daryl@newclarity.net>
2017-06-08 12:35:12 -0400

    Add missing build argument

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 declare -a versions
-versions=("1.2.1" "1.2.0" "1.1.9")
+versions=("1.2.1" "1.2.0" "1.1.0" "1.0.0")
 
 for version in ${versions[@]}; do
-  docker build -t wplib/wpcli:${version} ./
+  docker build -t wplib/wpcli:${version} --build-arg WPCLI_VERSION=${version} ./
 done
